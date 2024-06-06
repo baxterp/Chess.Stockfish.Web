0a434459a7cdf9ab67180da80a1b3192a19a618b
Baxter Pearson <baxterrpearson@gmail.com>
2024-06-06 13:41:19 +0100

    Project added

--- Chess.Stockfish.Web/Pages/Error.cshtml.cs
+++ Chess.Stockfish.Web/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Chess.Stockfish.WebClient.Pages
+namespace Chess.Stockfish.Web.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- Chess.Stockfish.Web/Pages/Privacy.cshtml.cs
+++ Chess.Stockfish.Web/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Chess.Stockfish.WebClient.Pages
+namespace Chess.Stockfish.Web.Pages
 {
     public class PrivacyModel : PageModel
     {
